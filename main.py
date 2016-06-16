ae8b701b4eadad3572140b6a2d42c8b6965e61fe
Ramon Saraiva <ramonsaraiva@gmail.com>
2016-06-16 02:17:41 -0300

    from trash to useful

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from flask import send_from_directory
 
 from flask.ext.restful import Api
 
-from models import db
-
-app = Flask(__name__)
-app.config.from_object(__name__)
-
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
 import daemon
 
+from models import db
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
 app.config.update({
-	'SQLALCHEMY_DATABASE_URI': 'postgresql://x:x@localhost/x'
+    'SQLALCHEMY_DATABASE_URI': 'postgresql://x:x@localhost/x'
 })
 
 db.init_app(app)
@@ -25,26 +25,26 @@ api = Api(app)
 
 @app.route('/')
 def send_template():
-	return send_from_directory('templates', 'base.html')
+    return send_from_directory('templates', 'base.html')
 
 @app.route('/<path:path>')
 def send_static(path):
-	return send_from_directory('static', path)
+    return send_from_directory('static', path)
 
 @app.cli.command()
 def drop():
-	db.drop_all()
+    db.drop_all()
 
 @app.cli.command()
 def create():
-	db.create_all()
+    db.create_all()
 
 @app.cli.command()
 def tornado():
-	log = open('tornado.log', 'a+')
-	ctx = daemon.DaemonContext(stdout=log, stderr=log, working_directory='.')
-	ctx.open()
+    log = open('tornado.log', 'a+')
+    ctx = daemon.DaemonContext(stdout=log, stderr=log, working_directory='.')
+    ctx.open()
 
-	http_server = HTTPServer(WSGIContainer(app))
-	http_server.listen(8080)
-	IOLoop.instance().start()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(8080)
+    IOLoop.instance().start()
--- static/app.js
+++ static/app.js
@@ -1,21 +1,17 @@
 'use strict';
 
 var app = angular.module('app', [
-	'ngRoute',
-	'controllers',
-	'services',
-	'directives',
+    'ngRoute',
+    'controllers',
+    'services'
 ]);
 
 app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider
-		.when('/home/', {
-			templateUrl: 'partials/home.tpl.html',
-		})
-		.when('/', {
-			redirectTo: '/home/'
-		})
-		.otherwise({
-			redirectTo: '/home/'
-		});
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.tpl.html',
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }]);
